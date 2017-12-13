fc740f482356e48623b48fb47f9ae38dedb43c8f
Emily Wells Jimenez and Susannah Lowe <emily+susannah@>
2017-12-13 15:30:00 -0800

    workaround for radio button form and angular syntax

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { Router } from '@angular/router';
+import { ProjectService } from '../project.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.scss']
+  styleUrls: ['./marketplace.component.scss'],
+  providers: [ProjectService]
 })
 export class MarketplaceComponent implements OnInit {
   projects: FirebaseListObservable<any[]>;
-  constructor() { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
   goToDetailPage(clickedProject) {
-
+    this.router.navigate(['projects', clickedProject.$key])
   };
-
 }
--- src/app/new-project-form/new-project-form.component.ts
+++ src/app/new-project-form/new-project-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProjectService } from '../project.service';
 import { Project } from '../project.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-project-form',
@@ -8,14 +9,15 @@ import { Project } from '../project.model';
   styleUrls: ['./new-project-form.component.scss'],
   providers: [ProjectService]
 })
-export class NewProjectFormComponent implements OnInit {
-
+export class NewProjectFormComponent {
+  categoryChoice: string ="Technology";
   constructor(private projectService: ProjectService) { }
 
-  ngOnInit() {
-  }
-  submitForm(title: string, description: string, organizers: string, goal: number, category: string, perks: string) {
-    var newProject: Project = new Project(title, description, organizers, goal, category, perks);
+
+  submitForm(title: string, description: string, organizers: string, goal: number,perks: string) {
+    console.log(this.categoryChoice);
+    var newProject: Project = new Project(title, description, organizers, goal, this.categoryChoice, perks);
+
     this.projectService.addProject(newProject);
   }
 }
